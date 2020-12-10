589bf28172485066aeb602094e62e55489878045
HassanP <lbe0418@gmail.com>
2020-12-10 13:36:10 +0900

    Create idol_story_list.js

---
+++ idol_story_list.js
@@ -0,0 +1,304 @@
+idol.idol_story_list = [{
+	"story_id": "428",
+	"story_title": "아이돌 프로듀스 the 9th Anniversary",
+	"voice_enable": ["1", "1"],
+	"flash_path": [
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/428/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=885320253",
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/428/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=205438126"
+	],
+	"voice_url": [
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/428/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=300062389",
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/428/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=233592722"
+	],
+	"open_flag": ["1", "1"],
+	"icon": [
+		"img/ui/episode/epi_button_movie_428_1.jpg",
+		"img/ui/episode/epi_button_movie_428_2.jpg"
+	],
+	"movie_name_voice": [
+		"img/ui/episode/epi_button_moviename_cool_special.png",
+		"img/ui/episode/epi_button_moviename_cool_special_2.png"
+	]
+}, {
+	"story_id": "424",
+	"story_title": "아이돌 프로듀스 the 9th Anniversary",
+	"voice_enable": ["1", "1"],
+	"flash_path": [
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/424/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=179344546",
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/424/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=96561520"
+	],
+	"voice_url": [
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/424/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=161282377",
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/424/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=626571817"
+	],
+	"open_flag": ["1", "1"],
+	"icon": [
+		"img/ui/episode/epi_button_movie_424_1.jpg",
+		"img/ui/episode/epi_button_movie_424_2.jpg"
+	],
+	"movie_name_voice": [
+		"img/ui/episode/epi_button_moviename_cool_early.png",
+		"img/ui/episode/epi_button_moviename_cool_late.png"
+	]
+}, {
+	"story_id": "420",
+    "story_title": "루미너스 스타(2020)",
+    "voice_enable": ["1", "1"],
+    "flash_path": [
+	    "http://125.6.169.35/idolmaster/idol_story/movie_play/420/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=117959544",
+	    "http://125.6.169.35/idolmaster/idol_story/movie_play/420/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=605998102"
+     ],
+    "voice_url": [
+    	"http://125.6.169.35/idolmaster/idol_story/sound_play/420/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=383469564",
+    	"http://125.6.169.35/idolmaster/idol_story/sound_play/420/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=350709384"
+	],
+    "open_flag": ["1", "1"],
+    "icon": [
+    	"img/ui/episode/101.jpg",
+    	"img/ui/episode/epi_button_movie_420_2.jpg"
+	],
+    "movie_name_voice": [
+    	"img/ui/episode/epi_button_moviename_cool_early.png",
+    	"img/ui/episode/epi_button_moviename_cool_late.png"
+	]
+}, {
+    "story_id": "412",
+    "story_title": "신데렐라 걸",
+    "voice_enable": ["1", "1"],
+    "flash_path": [
+    	"http://125.6.169.35/idolmaster/idol_story/movie_play/412/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=819010908",
+    	"http://125.6.169.35/idolmaster/idol_story/movie_play/412/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=381525609"
+	],
+    "voice_url": [
+    	"http://125.6.169.35/idolmaster/idol_story/sound_play/412/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=618439825",
+    	"http://125.6.169.35/idolmaster/idol_story/sound_play/412/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=488424516"
+	],
+    "open_flag": ["1", "1"],
+    "icon": [
+	    "img/ui/episode/101.jpg",
+	    "img/ui/episode/epi_button_movie_412_2.jpg"
+    ],
+    "movie_name_voice": [
+    	"img/ui/episode/epi_button_moviename_cool_early.png",
+    	"img/ui/episode/epi_button_moviename_cool_late.png"
+	]
+}, {
+	"story_id": "376",
+	"story_title": "루미너스 스타",
+	"voice_enable": ["1", "1"],
+	"flash_path": [
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/376/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=601515961",
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/376/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=721510963"
+	],
+	"voice_url": [
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/376/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=617427719",
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/376/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=738148798"
+	],
+	"open_flag": ["1", "1"],
+	"icon": [
+		"img/ui/episode/101.jpg",
+		"img/ui/episode/epi_button_movie_376_2.jpg"
+	],
+	"movie_name_voice": [
+		"img/ui/episode/epi_button_moviename_cool_early.png",
+		"img/ui/episode/epi_button_moviename_cool_late.png"
+	]
+}, {
+	"story_id": "353",
+	"story_title": "반짝임은 영원히♪ 트윙클 젬",
+	"voice_enable": {
+		"0": "1",
+		"3": "1"
+	},
+	"flash_path": {
+		"0": "https://lucid0418.github.io/mbga_ep/episode/51078468",
+		"3": "http://125.6.169.35/idolmaster/idol_story/movie_play/353/4/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=142547262"
+	},
+	"voice_url": {
+		"0": "http://125.6.169.35/idolmaster/idol_story/sound_play/353/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=624869423",
+		"3": "http://125.6.169.35/idolmaster/idol_story/sound_play/353/4/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=781555277"
+	},
+	"open_flag": {
+		"0": "1",
+		"3": "1"
+	},
+	"icon": {
+		"0": "img/ui/episode/101.jpg",
+		"3": "img/ui/episode/epi_button_movie_353_4.jpg"
+	},
+	"movie_name_voice": {
+		"0": "img/ui/episode/epi_button_moviename_cool_early.png",
+		"3": "img/ui/episode/epi_button_moviename_cool_late.png"
+	}
+}, {
+	"story_id": "266",
+	"story_title": "달콤함 듬뿍♪ 발렌타인 모드",
+	"voice_enable": {
+		"0": "1",
+		"3": "1"
+	},
+	"flash_path": {
+		"0": "http://125.6.169.35/idolmaster/idol_story/movie_play/266/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=650832425",
+		"3": "http://125.6.169.35/idolmaster/idol_story/movie_play/266/4/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=966603639"
+	},
+	"voice_url": {
+		"0": "http://125.6.169.35/idolmaster/idol_story/sound_play/266/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=210794473",
+		"3": "http://125.6.169.35/idolmaster/idol_story/sound_play/266/4/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=720909950"
+	},
+	"open_flag": {
+		"0": "1",
+		"3": "1"
+	},
+	"icon": {
+		"0": "img/ui/episode/101.jpg",
+		"3": "img/ui/episode/epi_button_movie_266_4.jpg"
+	},
+	"movie_name_voice": {
+		"0": "img/ui/episode/epi_button_moviename_cool_early.png",
+		"3": "img/ui/episode/epi_button_moviename_cool_late.png"
+	}
+}, {
+	"story_id": "217",
+	"story_title": "담채의 물방울",
+	"voice_enable": ["1", "1"],
+	"flash_path": [
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/217/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=291483310",
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/217/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=70655879"
+	],
+	"voice_url": [
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/217/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=740810483",
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/217/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=858449915"
+	],
+	"open_flag": ["1", "1"],
+	"icon": [
+		"img/ui/episode/101.jpg",
+		"img/ui/episode/epi_button_movie_217_2.jpg"
+	],
+	"movie_name_voice": [
+		"img/ui/episode/epi_button_moviename_cool_early.png",
+		"img/ui/episode/epi_button_moviename_cool_late.png"
+	]
+}, {
+	"story_id": "186",
+	"story_title": "텐더 크리스탈",
+	"voice_enable": ["1", "1"],
+	"flash_path": [
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/186/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=104443196",
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/186/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=825654611"
+	],
+	"voice_url": [
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/186/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=857793884",
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/186/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=183129141"
+	],
+	"open_flag": ["1", "1"],
+	"icon": [
+		"img/ui/episode/101.jpg",
+		"img/ui/episode/epi_button_movie_186_2.jpg"
+	],
+	"movie_name_voice": [
+		"img/ui/episode/epi_button_moviename_cool_early.png",
+		"img/ui/episode/epi_button_moviename_cool_late.png"
+	]
+}, {
+	"story_id": "156",
+	"story_title": "너에게 두근두근☆ 한여름의 비치 엔젤",
+	"voice_enable": {
+		"1": "1",
+		"4": "1"
+	},
+	"flash_path": {
+		"1": "http://125.6.169.35/idolmaster/idol_story/movie_play/156/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=976950511",
+		"4": "http://125.6.169.35/idolmaster/idol_story/movie_play/156/5/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=234227676"
+	},
+	"voice_url": {
+		"1": "http://125.6.169.35/idolmaster/idol_story/sound_play/156/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=898522325",
+		"4": "http://125.6.169.35/idolmaster/idol_story/sound_play/156/5/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=703284120"
+	},
+	"open_flag": {
+		"1": "1",
+		"4": "1"
+	},
+	"icon": {
+		"1": "img/ui/episode/epi_button_movie_karen.jpg",
+		"4": "img/ui/episode/epi_button_movie_156_5.jpg"
+	},
+	"movie_name_voice": {
+		"1": "img/ui/episode/epi_button_moviename_cool_early.png",
+		"4": "img/ui/episode/epi_button_moviename_cool_late.png"
+	}
+}, {
+	"story_id": "103",
+	"story_title": "제16회 드림LIVE페스티벌",
+	"voice_enable": ["1", "1"],
+	"flash_path": [
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/103/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=954791678",
+		"http://125.6.169.35/idolmaster/idol_story/movie_play/103/2/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=773117217"
+	],
+	"voice_url": [
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/103/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=179248241",
+		"http://125.6.169.35/idolmaster/idol_story/sound_play/103/2/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=847308332"
+	],
+	"open_flag": ["1", "1"],
+	"icon": [
+		"img/ui/episode/epi_button_movie_karen.jpg",
+		"img/ui/episode/epi_button_movie_103_2.jpg"
+	],
+	"movie_name_voice": [
+		"img/ui/episode/epi_button_moviename_cool_early.png",
+		"img/ui/episode/epi_button_moviename_cool_late.png"
+	]
+}, {
+	"story_id": "62",
+	"story_title": "3주년 기념 크리스마스 파티",
+	"voice_enable": {
+		"2": "1",
+		"5": "1"
+	},
+	"flash_path": {
+		"2": "http://125.6.169.35/idolmaster/idol_story/movie_play/62/3/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=811630529",
+		"5": "http://125.6.169.35/idolmaster/idol_story/movie_play/62/6/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=757162541"
+	},
+	"voice_url": {
+		"2": "http://125.6.169.35/idolmaster/idol_story/sound_play/62/3/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=439093781",
+		"5": "http://125.6.169.35/idolmaster/idol_story/sound_play/62/6/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=795581973"
+	},
+	"open_flag": {
+		"2": "1",
+		"5": "1"
+	},
+	"icon": {
+		"2": "img/ui/episode/epi_button_movie_karen.jpg",
+		"5": "img/ui/episode/epi_button_movie_062_6.jpg"
+	},
+	"movie_name_voice": {
+		"2": "img/ui/episode/epi_button_moviename_cool_early.png",
+		"5": "img/ui/episode/epi_button_moviename_cool_late.png"
+	}
+}, {
+	"story_id": "33",
+	"story_title": "아이돌 7변화☆ 영업 챌린지",
+	"voice_enable": {
+		"0": "1",
+		"3": "1"
+	},
+	"flash_path": {
+		"0": "http://125.6.169.35/idolmaster/idol_story/movie_play/33/1/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=957198403",
+		"3": "http://125.6.169.35/idolmaster/idol_story/movie_play/33/4/idol_gallery--idol_detail--__hash_card_id__--2?l_frm=Idol_gallery_idol_detail_1&rnd=523375873"
+	},
+	"voice_url": {
+		"0": "http://125.6.169.35/idolmaster/idol_story/sound_play/33/1/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=668991959",
+		"3": "http://125.6.169.35/idolmaster/idol_story/sound_play/33/4/idol_gallery/__hash_card_id__?l_frm=Idol_gallery_idol_detail_1&rnd=836677310"
+	},
+	"open_flag": {
+		"0": "1",
+		"3": "1"
+	},
+	"icon": {
+		"0": "img/ui/episode/epi_button_movie_karen.jpg",
+		"3": "img/ui/episode/epi_button_movie_033_4.jpg"
+	},
+	"movie_name_voice": {
+		"0": "img/ui/episode/epi_button_moviename_cool_early.png",
+		"3": "img/ui/episode/epi_button_moviename_cool_late.png"
+	}
+}];
